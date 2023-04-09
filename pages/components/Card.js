84e13cbc94bc060567c42fbf062e1100e6bc7c4f
jocyanno <jocyannovittor@hotmail.com>
2023-04-09 14:16:59 -0300

    first commit

---
+++ pages/components/Card.js
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+
+export default function Card ({pokemon}) {
+    return (
+        <div>
+             <Image
+                src={`https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`}
+                width="120"
+                height="120"
+                alt={pokemon.name}
+            />
+            <p>#{pokemon.id}</p>
+            <h3>
+                {pokemon.name}
+            </h3>
+        </div>
+        )
+}
